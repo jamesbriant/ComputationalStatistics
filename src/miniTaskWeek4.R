03c144156df50dd4bf34575818e0fe4661a2180d
diggerb <james@briant.co.uk>
2020-10-14 21:42:42 +0100

    Create miniTaskWeek4.R

---
+++ src/miniTaskWeek4.R
@@ -0,0 +1,26 @@
+f <- function(x, y){
+  a <- (1-x)^2
+  b <- y-x^2
+  return(a + 100*b^2)
+}
+
+fxy <- function(x, y){
+  # x & y should be vectors of the same length!
+  m <- length(x)
+  n <- length(y)
+  
+  outputMatrix <- matrix(0, nrow=m, ncol=n)
+  
+  for(i in 1:m){
+    outputMatrix[i, ] <- sapply(x[i], f, y)
+  }
+  
+  return(outputMatrix)
+}
+
+x <- seq(from=-2, to=2, length.out=100)
+y <- seq(from=-2, to=5, length.out=175)
+z <- fxy(x, y)
+
+persp(x, y, z)
+contour(x, y, z)
